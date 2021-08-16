0867d7c5376427ee9a988b86bfa6ee20f3a745d3
Rectiras <soyluberkay@sabanciuniv.edu>
2021-08-16 19:30:30 +0300

    Layout html created and settings.py updated

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the mainpage.")
+    return render(request, 'index.html')
 # Create your views here.
