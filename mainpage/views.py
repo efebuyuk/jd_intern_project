5b62392d8c236f901bcc533a9f7e8d761b8c7987
Rectiras <soyluberkay@sabanciuniv.edu>
2021-08-17 14:16:34 +0300

    User Registration Works

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,14 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
-
 def signup(request):
-    return render(request, 'signup.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form':form}
+
+    return render(request, 'signup.html', context)
+
+def login(request):
+    context = {}
+
+    return render(request, 'login.html', context)
+
